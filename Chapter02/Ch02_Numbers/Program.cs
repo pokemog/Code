bff9996647fd842c920014ec334721047b714054
Minh Trang <matrang@hotmail.com>
2017-09-17 17:26:06 -0700

    Adding Chapter 02

---
+++ Chapter02/Ch02_Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ch02_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+            Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+            Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
+
+            double a = 0.1;
+            double b = 0.2;
+            decimal c = 0.1m;
+            decimal d = 0.2m;
+            if (a + b == 0.3)
+            {
+                Console.WriteLine($"Double type {a} + {b} equals 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"Double type {a} + {b} does NOT equal 0.3");
+            }
+
+            if (c + d == 0.3m)
+            {
+                Console.WriteLine($"Decimal type {c} + {d} equals 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"Decimal type {c} + {d} does NOT equal 0.3");
+            }
+        }
+    }
+}
---
+++ Chapter02/Ch02_Variables/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ch02_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object height = 1.88; // storing a double in an object
+            object name = "Amir"; // storing a string in an object
+            //int length1 = name.Length; // gives compile error!
+            int length2 = ((string)name).Length; //cast to access members
+
+            // storing a string in a dynamic object
+            dynamic anotherName = "Ahmed";
+            // this compiles but might throw an exception at run-time!
+            int length = anotherName.Length;
+
+            var population = 66_000_000; // 66 million in UK
+            var weight = 1.88; // in kilograms
+            var price = 4.99M; // in pounds sterling
+            var fruit = "Apples"; // strings use double-quotes
+            var letter = 'Z'; //chars use single-quotes
+            var happy = true; // Booleans have value of true or false
+
+            Console.WriteLine($"{default(int)}"); // 0
+            Console.WriteLine($"{default(bool)}"); // False
+            Console.WriteLine($"{default(DateTime)}"); // 1/01/0001 00:00:00
+        }
+    }
+}
