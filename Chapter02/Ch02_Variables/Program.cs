98277d3c3dd9f3e3ccd3b51d64051c0251a9a2ca
Minh Trang <matrang@hotmail.com>
2017-10-15 16:08:46 -0700

    Adding outputs for local variables

--- Chapter02/Ch02_Variables/Program.cs
+++ Chapter02/Ch02_Variables/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Ch02_Variables
 {
@@ -6,16 +7,19 @@ namespace Ch02_Variables
     {
         static void Main(string[] args)
         {
+            // The object type
             object height = 1.88; // storing a double in an object
             object name = "Amir"; // storing a string in an object
             //int length1 = name.Length; // gives compile error!
             int length2 = ((string)name).Length; //cast to access members
 
+            // The dynamic type
             // storing a string in a dynamic object
             dynamic anotherName = "Ahmed";
             // this compiles but might throw an exception at run-time!
             int length = anotherName.Length;
 
+            // Inferring the type of a local variable
             var population = 66_000_000; // 66 million in UK
             var weight = 1.88; // in kilograms
             var price = 4.99M; // in pounds sterling
@@ -23,15 +27,51 @@ namespace Ch02_Variables
             var letter = 'Z'; //chars use single-quotes
             var happy = true; // Booleans have value of true or false
 
-            Console.WriteLine($"{default(int)}"); // 0
-            Console.WriteLine($"{default(bool)}"); // False
-            Console.WriteLine($"{default(DateTime)}"); // 1/01/0001 00:00:00
+            // Displaying output to the user
+            Console.WriteLine($"The UK population is {population}.");
+            Console.WriteLine($"The UK population is {population:N0}. ");
+            Console.WriteLine($"{weight}kg of {fruit} costs {price:C}.");
 
+            WriteLine($"{default(int)}"); // 0
+            WriteLine($"{default(bool)}"); // False
+            WriteLine($"{default(DateTime)}"); // 1/01/0001 00:00:00
+
+            // Making a value type nullable
             int iCanNotBeNull = 4;
             int? iCanBeNull = null;
-            Console.WriteLine(iCanBeNull.GetValueOrDefault());
+            WriteLine(iCanBeNull.GetValueOrDefault());
             iCanBeNull = 4;
-            Console.WriteLine(iCanBeNull.GetValueOrDefault());
+            WriteLine(iCanBeNull.GetValueOrDefault());
+
+            // Checking for null
+            // Check is myVariable is not null before using it
+            if (iCanBeNull != null)
+            {
+                WriteLine($"{nameof(iCanBeNull)} is not null");
+            }
+
+            // Checking for null for variable field or property
+            string authorName = null;
+            // if authorName is null, instead of throwing an exception,
+            // null is returned
+            int? howManyLetters = authorName?.Length;
+            WriteLine($"Variable {nameof(howManyLetters)} is: {howManyLetters}");
+            // next line should throw an NullException
+            //int? howManyLetters = authorName.Length;
+
+            // result will be three if howManyLetters is null
+            var result = howManyLetters ?? 3;
+            WriteLine($"Variable {nameof(result)} is: {result}");
+
+            // Storing multiples values in an array
+            // declaring the size of the array
+            string[] names = new string[] { "Daddy", "Mommy", "Baby" };
+            WriteLine($"The length of {nameof(names)} variable is: {names.Length}");
+            for (int i = 0; i < names.Length; i++)
+            {
+                // read the itemat this index
+                WriteLine(names[i]);
+            }
         }
     }
 }
