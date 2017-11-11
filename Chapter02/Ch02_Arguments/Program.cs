795af35ea93387f3605352ddfe519ca5473e871a
Minh Trang <matrang@hotmail.com>
2017-11-11 15:42:37 -0800

    Adding exception handling

--- Chapter02/Ch02_Arguments/Program.cs
+++ Chapter02/Ch02_Arguments/Program.cs
@@ -7,11 +7,27 @@ namespace Ch02_Arguments
     {
         static void Main(string[] args)
         {
-            ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), args[0], true);
-            BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), args[1], true);
-            WindowWidth = int.Parse(args[2]);
-            WindowHeight = int.Parse(args[3]);
+            try {
+                ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), args[0], true);
+                BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), args[1], true);
+                WindowWidth = int.Parse(args[2]);
+                WindowHeight = int.Parse(args[3]);
+            }
+            catch (IndexOutOfRangeException e) {
+                WriteLine("One or more arguments are missing!");
+                WriteLine($"Stacktrace: {e}");
+            }
+            // Handling platforms that do not support an API
+            // WindowWidth, WindowHeight
+            catch (PlatformNotSupportedException) {
+                WriteLine("The current platform doe not support chaning the size of a console window.");
+            }
             
+            DisplayArguments(args);
+        }
+
+        // Enumerating agurments
+        private static void DisplayArguments(string[] args) {
             WriteLine($"There are {args.Length} arguments.");
             foreach (var arg in args)
             {
